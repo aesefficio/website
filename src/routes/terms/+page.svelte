e586de1b0475df2d3109e55857d34ac47017bede
Alexander <i@alexander.systems>
2024-06-18 11:33:35 -0500

    Create terms of service

---
+++ src/routes/terms/+page.svelte
@@ -0,0 +1,43 @@
+
+<svelte:head>
+    <title>Terms of Service</title> 
+    <meta name="description" content="Aes Efficio Terms of Service">
+    <meta name="keywords" content="AesEfficio, Efficio, Software">
+    <meta name="author" content="Alexander Akira Weimer">
+    <meta name="copyright" content="Alexander Akira Weimer" />
+    <meta property="og:site_name" content="Aes Efficio" />
+    <meta property="og:image" content="/img/wordmark.png" /> 
+    <meta property="fb:admins" content="268094773018996" />
+</svelte:head>
+
+<main class="justify-between items-center l-0 drop-shadow-xl">
+    <div class="flex flex-col h-[100vh] pt-2 md:pt-4 w-full">
+        <div class="flex justify-center font-bold font-varela text-white text-sm hover:underline decoration-dotted ">
+            <a href="../">home</a>
+        </div>
+        <div class="pt-[20vh] max-w-[512px] px-6 mx-auto text-center justify-center font-varela text-white text-sm">
+            <h1 class="text-3xl pb-10 font-mastery">privacy</h1>
+            <p class="pb-3">
+                By accessing or using software provided by Aes Efficio, you agree to be bound by the following terms.
+            </p>
+            <p class="pb-3">
+              You agree to use software provided by Aes Efficio in compliance with all applicable laws and regulations. You are responsible for any activity that occurs as a result of your use of our software.
+            </p>
+            <p class="pb-3">
+              Aes Efficio provides software "as is" and "as available" without any warranties of any kind, either express or implied. We do not guarantee that the app will be available at all times or free from errors.           
+            </p>
+   <p class="pb-3">
+To the maximum extent permitted by law, Aes Efficio shall not be liable for any direct, indirect, incidental, special, consequential, or exemplary damages, including but not limited to, damages for loss of profits, goodwill, use, data, or other intangible losses arising out of or in connection with your use of our software.
+     </p>
+                 <p class="pb-3">
+We reserve the right to modify these Terms of Service at any time. Any changes will be effective immediately upon posting. Your continued use of the app after changes are posted constitutes your acceptance of the revised terms.
+                   </p>                <p class="pb-3">
+                This page was last updated <span class="underline decoration-dotted">18 June 2024</span>. We reserve the right to update or change these terms at any time. Any changes will be posted on this page with an updated effective date. We encourage you to review this Privacy Policy periodically for any changes.
+            </p>
+            <p class="pb-3">
+                For any concerns or questions, please contact us on <a href="https://discord.com/invite/kw7hx73wVA" class="underline decoration-dotted">Discord</a> or via Email at <span class="underline decoration-dotted">aaw «аt» aes «dot» rip</span>
+            </p>
+        </div>
+    </div>
+
+</main>
