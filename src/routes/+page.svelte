0559a1b620f74eea9c7fe44dd5da3f915b41ae3f
Alexander Weimer <i@alexander.systems>
2026-07-07 17:31:21 -0500

    Modify footer links in +page.svelte
    Updated footer links to include Crunchbase and company information.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -108,6 +108,14 @@
 </svg>
             </span>
         <span class="w-full pt-[10vh] text-center justify-center flex items-center md:text-center font-bold font-mastery text-fuchsia-50 pb-4 md:pb-8">
+<a class="ml-8 relative group" href="https://www.linkedin.com/company/99153631" target="_blank" rel="noopener noreferrer">
+    <svg width="32px" height="32px" class="linkicon inline cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <path d="M100.28 448H7.4V148.9h92.88zm-46.44-340.7C24.09 107.3 0 83.2 0 53.5A53.5 53.5 0 0 1 53.84 0c29.7 0 53.5 24.1 53.5 53.8 0 29.4-23.8 53.5-53.5 53.5zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/>
+    </svg>
+    <svg class="absolute -top-1 -right-1 w-3 h-3 text-fuchsia-100 transform group-hover:scale-[135%] group-hover:translate-x-[2px] group-hover:-translate-y-[2px] transition-all" fill="currentColor" viewBox="0 0 12 12">
+        <path d="M3.5 3L8.5 3L8.5 8M8.5 3L3.5 8"/>
+    </svg>
+</a>
             <a class="mr-8 relative group" data-text="@aesefficio" href="https://github.com/aesefficio" target="_blank" rel="noopener noreferrer">
                 <svg width="32px" height="32px" class="linkicon inline cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
                     <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
@@ -219,6 +227,9 @@
                                         </div>
                                     </div>
                                 </div>
+<p class="mt-6 text-center text-sm md:text-base font-varela text-fuchsia-100/80 max-w-xl mx-auto">
+Aes Efficio is a development team that creates interactive software experiences with over 50 million downloads to date. It is led by founder & lead developer Alexander Weimer.
+</p>
             
                 
                                 <div class="bg-gray-800 notched-corners p-6 hover:bg-gray-700 transition-colors duration-300">
@@ -259,9 +270,19 @@
         </div>
     </section>
 -->
-    <div class="fixed inset-x-0 bottom-3 flex justify-center font-bold font-varela text-white text-sm hover:underline decoration-dotted ">
-        <a href="./privacy">privacy policy</a>
-    </div>
+    <div class="fixed inset-x-0 bottom-3 flex justify-center font-bold font-varela text-white text-sm hover:underline decoration-dotted">
+    <a href="https://www.crunchbase.com/organization/aes-efficio" target="_blank" rel="noopener noreferrer">
+        crunchbase
+    </a>
+    <span class="mx-3 text-fuchsia-200">◆</span>
+    <a href="https://www.dnb.com/business-directory/company-profiles.aes_efficio_llc.e75b0a3a5f93d2a88070528b66a978b8.html" target="_blank" rel="noopener noreferrer">
+        company information
+    </a>
+    <span class="mx-3 text-fuchsia-200">◆</span>
+    <a href="./privacy">
+        privacy policy
+    </a>
+</div>
     </main>
 
 <style> 
